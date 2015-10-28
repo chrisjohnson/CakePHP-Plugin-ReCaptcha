af4cb2f14432fea2d2082632a432677d8d5473c8
Chris Johnson <chrisjohnson0@gmail.com>
2015-10-28 19:20:34 +0000

    Update ReCaptchaToken to use NTP to look up time when no time is given

--- Lib/ReCaptchaToken.php
+++ Lib/ReCaptchaToken.php
@@ -1,6 +1,8 @@
 <?php
 class ReCaptchaToken {
 	public static $_client;
+	public static $_client_ntp;
+
 	public static function client() {
 		if (empty(self::$_client)) {
 			require(App::pluginPath('ReCaptcha') . 'Vendor/recaptcha-secure-token/lib/ReCaptchaToken.php');
@@ -12,11 +14,26 @@ class ReCaptchaToken {
 
 		return self::$_client;
 	}
-	public static function secureToken($sessionId = null) {
+
+	public static function client_ntp() {
+		if (empty(self::$_client_ntp)) {
+			require(App::pluginPath('ReCaptcha') . 'Vendor/ntp/src/Client.php');
+			require(App::pluginPath('ReCaptcha') . 'Vendor/ntp/src/Socket.php');
+			$socket = new \Bt51\NTP\Socket('0.pool.ntp.org', 123);
+			self::$_client_ntp = new \Bt51\NTP\Client($socket);
+		}
+
+		return self::$_client_ntp;
+	}
+
+	// Generate a unique secureToken based on a unique sessionId, and an accurate timestamp. Leave timestamp empty to look to ntp server instead of using local time
+	public static function secureToken($sessionId = null, $timestamp = null) {
 		if (empty($sessionId)) {
 			$sessionId = uniqid('recaptcha-session');
 		}
-		$timestamp = round(microtime(true) * 1000) - (30 * 1000); // Timestamp from 30 seconds ago, in case of local server time being ahead of google time (ugh.)
+		if (empty($timestamp)) {
+			$timestamp = self::client_ntp()->getTime()->getTimestamp() * 1000;
+		}
 		return self::client()->secureToken($sessionId, $timestamp);
 	}
 }
