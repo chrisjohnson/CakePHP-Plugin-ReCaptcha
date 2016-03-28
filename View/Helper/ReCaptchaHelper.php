a0d94672d9345ad2f597b0f65509eada8326756a
Chris Johnson <chrisjohnson0@gmail.com>
2016-03-28 22:55:45 +0000

    Return message when missing recaptcha info

--- View/Helper/ReCaptchaHelper.php
+++ View/Helper/ReCaptchaHelper.php
@@ -4,6 +4,9 @@ class ReCaptchaHelper extends Helper {
 	public $uses = ['Html'];
 	public function display() {
 		$site_key = Configure::read('ReCaptcha.site_key');
+		if (empty($site_key)) {
+			return 'ReCaptcha Not Configured';
+		}
 		$secure_token = ReCaptchaToken::secureToken();
 		$script = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
 		return sprintf('<div class="g-recaptcha" data-sitekey="%s" data-stoken="%s"></div>%s', $site_key, $secure_token, $script);
