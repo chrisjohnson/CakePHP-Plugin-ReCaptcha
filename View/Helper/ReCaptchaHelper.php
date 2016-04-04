0c717c82fa7a7c13851d16fbfff6a1932bd96311
Chris Johnson <chrisjohnson0@gmail.com>
2016-04-04 20:33:04 +0000

    Make secure token generation an option

--- View/Helper/ReCaptchaHelper.php
+++ View/Helper/ReCaptchaHelper.php
@@ -2,13 +2,14 @@
 App::uses('ReCaptchaToken', 'ReCaptcha.Lib');
 class ReCaptchaHelper extends Helper {
 	public $uses = ['Html'];
-	public function display() {
+	public function display($secureToken = false) {
 		$site_key = Configure::read('ReCaptcha.site_key');
 		if (empty($site_key)) {
 			return 'ReCaptcha Not Configured';
 		}
-		$secure_token = ReCaptchaToken::secureToken();
+		// Use Secure Token setup
+		$secure_token = $secureToken ? sprintf('data-stoken="%s"', ReCaptchaToken::secureToken()) : '';
 		$script = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
-		return sprintf('<div class="g-recaptcha" data-sitekey="%s" data-stoken="%s"></div>%s', $site_key, $secure_token, $script);
+		return sprintf('<div class="g-recaptcha" data-sitekey="%s" %s></div>%s', $site_key, $secure_token, $script);
 	}
 }
