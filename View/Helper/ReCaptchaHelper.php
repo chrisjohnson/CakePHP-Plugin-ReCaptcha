cd30c7520e2b2cbf081ff29a06a8ea6c5d2b0150
Chris Johnson <chrisjohnson0@gmail.com>
2016-06-03 20:56:57 +0000

    Only include the api script once

--- View/Helper/ReCaptchaHelper.php
+++ View/Helper/ReCaptchaHelper.php
@@ -2,6 +2,7 @@
 App::uses('ReCaptchaToken', 'ReCaptcha.Lib');
 class ReCaptchaHelper extends Helper {
 	public $uses = ['Html'];
+	public static $includedScript = false;
 	public function display($secureToken = false) {
 		$site_key = Configure::read('ReCaptcha.site_key');
 		if (empty($site_key)) {
@@ -9,7 +10,12 @@ class ReCaptchaHelper extends Helper {
 		}
 		// Use Secure Token setup
 		$secure_token = $secureToken ? sprintf('data-stoken="%s"', ReCaptchaToken::secureToken()) : '';
-		$script = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
+		if (!self::$includedScript) {
+			self::$includedScript = true;
+			$script = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
+		} else {
+			$script = '';
+		}
 		return sprintf('<div class="g-recaptcha" data-sitekey="%s" %s></div>%s', $site_key, $secure_token, $script);
 	}
 }
